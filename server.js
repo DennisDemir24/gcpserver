1641e2217512fabcce13a71fcffe80278f13a930
Dennis Demir <dd222gc@student.lnu.se>
2021-12-02 14:24:59 +0100

    routes

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const datastore = ds.datastore
 const CUSTOMERS = 'customers'
 
 
-// Get customers
+// Get customer name and id
 app.get('/getCustomers', (req, res) => {
-  const query = datastore.createQuery(CUSTOMERS).select(['name'])
+  const query = datastore
+    .createQuery(CUSTOMERS)
+    .select(['name', 'id'])
   datastore
     .runQuery(query)
     .then((results) => {
@@ -20,6 +22,7 @@ app.get('/getCustomers', (req, res) => {
       res.status(500).send(err)
     })
 })
+
 
 // Get customer by id
 app.get('/getCustomer/:id', (req, res) => {
@@ -34,20 +37,6 @@ app.get('/getCustomer/:id', (req, res) => {
     })
     
 })
-
-/* app.get('/getCustomerswithproperty', (req, res) => {
-  const query = datastore.createQuery(CUSTOMERS).select(['name'])
-    datastore
-      .runQuery(query)
-      .then((results) => {
-        const customers = results[0]
-        res.send(customers)
-      })
-      .catch((err) => {
-        console.error('ERROR:', err)
-        res.status(500).send(err)
-      })
-}) */
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080
